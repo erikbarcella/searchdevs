d2023d6af4e7af5471b0bffbd21a8d5314cb7f2f
Eriik <57962093+erikbarcella@users.noreply.github.com>
2024-01-16 11:35:55 -0300

    exibicao de infos e repos do usuario
    exibicao simples de infos do usuario

--- searchdevs/src/Pages/Home.jsx
+++ searchdevs/src/Pages/Home.jsx
@@ -1,23 +1,36 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Search from '../Components/Search'
+import axios from 'axios';
+import './Perfil'
 
 export function Home() {
     const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
 
-    const handleSearch = () => {
-        console.log(searchTerm)
-        console.log(`Pesquisando por: ${searchTerm}`);
-      };
+    const handleSearch = async () => {
+        try {
+            const res = await axios.get(`https://api.github.com/users/${searchTerm}`)
+            if(res && res.data && res.status === 200) {
+                try {
+                    const response = await axios.get(`https://api.github.com/users/${searchTerm}/repos`)
+                    if(response && response.data && response.status === 200) {
+                        navigate('/perfil', { state: { user: res.data, repos: response.data } });
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
-        <>
         <div className='home'> 
-        <h1><span className="search-color">Search </span><span className="devs-color">d_evs</span></h1>
+            <h1><span className="search-color">Search </span><span className="devs-color">d_evs</span></h1>
             <Search onChange={e => setSearchTerm(e.target.value)}/>
             <button onClick={handleSearch}>Search</button>
         </div>
-            
-
-        </>
     )
 }
--- searchdevs/src/Pages/Perfil.jsx
+++ searchdevs/src/Pages/Perfil.jsx
@@ -1,8 +1,19 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 export function Perfil() {
+    const location = useLocation();
+    const { user, repos } = location.state;
 
     return (
-        <>
-            <h1>Perfil</h1>
-        </>
-    )
+        <div>
+            <h1>{user.name}</h1>
+            <p>{user.bio}</p>
+            <ul>
+                {repos.map(repo => (
+                    <li key={repo.id}>{repo.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
