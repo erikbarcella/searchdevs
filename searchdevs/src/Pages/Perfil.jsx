d2095f43d86b3fc3f4ab81b325776e2cefe3d4c4
Erik Barcella <agenciaebdigital@gmail.com>
2024-01-17 00:28:51 -0300

    renderização dinamica perfil
    implementada renderização dinamica no perfil

--- searchdevs/src/Pages/Perfil.jsx
+++ searchdevs/src/Pages/Perfil.jsx
@@ -1,51 +1,85 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useLocation } from 'react-router-dom';
 import Search from '../Components/Search'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export function Perfil() {
     const location = useLocation();
     const { user, repos, searchTerm } = location.state;
-    const [localSearchTerm, setLocalSearchTerm] = useState('');    
+    const [localSearchTerm, setLocalSearchTerm] = useState('');
+    const [searchRepos, setSearchRepos] = useState([]);
+    const [searchUsers, setSearchUser] = useState('');
+    const navigate = useNavigate();
 
-    const handleSearch = async () => {
-        // try {
-        //     const res = await axios.get(`https://api.github.com/users/${searchTerm}`)
-        //     if(res && res.data && res.status === 200) {
-        //         try {
-        //             const response = await axios.get(`https://api.github.com/users/${searchTerm}/repos`)
-        //             if(response && response.data && response.status === 200) {
-        //                 // navigate('/perfil', { state: { user: res.data, repos: response.data, searchTerm } });
-        //             }
-        //         } catch (error) {
-        //             console.log(error);
-        //         }
-        //     }
-        // } catch (error) {
-        //     console.log(error);
-        // }
-    }
+    const handleSearch = useCallback(async () => {
+        try {
+            const res = await axios.get(`https://api.github.com/users/${searchTerm}`)
+            if (res && res.data && res.status === 200) {
+                setSearchUser(res.data);
+                try {
+                    const response = await axios.get(`https://api.github.com/users/${searchTerm}/repos`)
+                    if (response && response.data && response.status === 200) {
+                        setSearchRepos(response.data);
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }, [searchTerm]);
+
+    const handleClick = async () => {
+        await handleSearch();
+        navigate(location.pathname, { state: { user, repos, searchTerm: localSearchTerm } });
+    };
 
     useEffect(() => {
         if (localSearchTerm !== '') {
-          // Chama a função de busca quando localSearchTerm não estiver vazio
-          handleSearch();
+            handleSearch();
         }
-      }, [localSearchTerm]);
+    }, [handleSearch, localSearchTerm]);
 
     return (
         <div>
-              
-             
-            <h1><span className="search-color">Search </span><span className="devs-color">d_evs</span></h1>
-            <Search value={localSearchTerm}  placeholder={searchTerm} onChange={e => setLocalSearchTerm(e.target.value)}/>
-            <button onClick={handleSearch} >Search</button>
-            <h1>{user.name}</h1>
-            <p>{user.bio}</p>
-            <ul>
-                {repos.map(repo => (
-                    <li key={repo.id}>{repo.name}</li>
-                ))}
-            </ul>
+            <div>
+                <h1>
+                    <span className="search-color">Search </span>
+                    <span className="devs-color">d_evs</span>
+                </h1>
+                <Search
+                    value={localSearchTerm}
+                    placeholder={localSearchTerm ? localSearchTerm : searchTerm}
+                    onChange={(e) => setLocalSearchTerm(e.target.value)}
+                />
+                <button onClick={handleClick}>Search</button>
+            </div>
+
+            {searchUsers ? (
+                <div>
+                    <h2>{searchUsers.name}</h2>
+                    <img src={searchUsers.avatar_url} alt="Avatar" />
+                    <h3>Repos</h3>
+                    <ul>
+                        {searchRepos.map((repo) => (
+                            <li key={repo.id}>{repo.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <div>
+                    <h2>{user.name}</h2>
+                    <img src={user.avatar_url} alt="Avatar" />
+                    <h3>Repos</h3>
+                    <ul>
+                        {repos.map((repo) => (
+                            <li key={repo.id}>{repo.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
